ea6d6840a9ba0b3e941a3804b57cc239a9b60ffc
a-singh09 <a_singh09@yahoo.com>
2025-09-23 20:16:29 +0530

    [Feat] Create InputSection component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,10 @@
 /**
  * Component exports for the Birthday Calendar application
- * Components will be implemented in subsequent tasks
  */
 
 // Re-export types for convenience
 export * from "../types";
 
-// Placeholder for component exports that will be implemented in later tasks
+// Component exports
+export { InputSection } from "./InputSection";
+// Additional components will be exported as they are implemented
